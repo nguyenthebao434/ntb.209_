5bbcf814468e1e1d7d07e39dae058774c5582ddc
nguyenthebao434 <160803417+nguyenthebao434@users.noreply.github.com>
2026-09-10 16:19:53 +0700

    Update baodeptrai.lua

--- baodeptrai.lua
+++ baodeptrai.lua
@@ -1,1 +1,47 @@
-.
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Bảo Đẹp Trai",
+         Animation = "Youtube: Bảo Đẹp Trai"
+         },
+        Key = {
+        KeySystem = true,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"baodeptrai"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "https://www.pinterest.com/pin/677158493996762996/",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Script Farm"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "RealKid Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://pastebin.com/raw/Hc2ZvUN5"))()(Settings)
+  end
+  })
