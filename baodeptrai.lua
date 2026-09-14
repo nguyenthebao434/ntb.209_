a1d8bb602a1402eae9aa0f41b09280119a265696
nguyenthebao434 <160803417+nguyenthebao434@users.noreply.github.com>
2026-09-14 17:16:07 +0700

    Update baodeptrai.lua

--- baodeptrai.lua
+++ baodeptrai.lua
@@ -43,5 +43,20 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
 }
 
 loadstring(game:HttpGet("https://pastebin.com/raw/Hc2ZvUN5"))()(Settings)
+
+------ Tab
+     local Tab1o = MakeTab({Name = "Script Farm"})
+
+------- BUTTON
+
+    AddButton(Tab1o, {
+     Name = "RealKid Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://pastebin.com/raw/Hc2ZvUN5"))()(Settings)
   end
   })
